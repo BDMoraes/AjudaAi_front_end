470500f7d6a2a6af379492cf06b157e8861ad2c6
Tales K <37679931+TalesRK@users.noreply.github.com>
2021-10-06 19:39:14 -0300

    feat: added coreui template to project

---
+++ src/views/pages/register/Register.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilPhone, cilText, cilUser } from '@coreui/icons'
+
+const Register = () => {
+  const [form, setForm] = useState({
+    username: '',
+  })
+
+  const handleInputChange = (name, value) => {
+    console.log({ name, value })
+    // setInputValues((currentValues) => ({
+    //   ...currentValues,
+    //   [name]: selectedDriver.saldo,
+    // }))
+
+    setForm((actualForm) => ({
+      ...actualForm,
+      [name]: value,
+    }))
+  }
+
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={9} lg={7} xl={6}>
+            <CCard className="mx-4">
+              <CCardBody className="p-4">
+                <CForm>
+                  <h1>Cadastro</h1>
+                  <p className="text-medium-emphasis">Crie sua conta</p>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput
+                      placeholder="Usuário"
+                      autoComplete="username"
+                      value={form.username}
+                      onChange={(event) => handleInputChange('username', event.target.value)}
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilText} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Nome completo" autoComplete="username" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilPhone} />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Telefone" autoComplete="username" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormInput placeholder="Email" autoComplete="email" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput type="password" placeholder="Senha" autoComplete="new-password" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-4">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="password"
+                      placeholder="Confirmar senha"
+                      autoComplete="new-password"
+                    />
+                  </CInputGroup>
+                  <div className="d-grid">
+                    <CButton color="success">Criar conta</CButton>
+                  </div>
+                </CForm>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Register
