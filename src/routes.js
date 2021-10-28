80704f6a333fd4c2e3bba46d997e9c5c423c3100
Bruno Moraes <brunodelciomoraes@gmail.com>
2021-10-27 21:23:42 -0300

    cadastro de eventos

--- src/routes.js
+++ src/routes.js
@@ -38,7 +38,7 @@ const Charts = React.lazy(() => import('./views/charts/Charts'))
 
 // Icons
 const CoreUIIcons = React.lazy(() => import('./views/icons/coreui-icons/CoreUIIcons'))
-const Flags = React.lazy(() => import('./views/icons/flags/Flags'))
+const Register_events = React.lazy(() => import('./views/icons/flags/Register_events'))
 const Brands = React.lazy(() => import('./views/icons/brands/Brands'))
 
 // Notifications
@@ -85,7 +85,7 @@ const routes = [
   { path: '/forms/validation', name: 'Validation', component: Validation },
   { path: '/icons', exact: true, name: 'Icons', component: CoreUIIcons },
   { path: '/icons/coreui-icons', name: 'CoreUI Icons', component: CoreUIIcons },
-  { path: '/icons/flags', name: 'Flags', component: Flags },
+  { path: '/icons/register-events', name: 'Register Events', component: Register_events },
   { path: '/icons/brands', name: 'Brands', component: Brands },
   { path: '/notifications', name: 'Notifications', component: Alerts, exact: true },
   { path: '/notifications/alerts', name: 'Alerts', component: Alerts },
